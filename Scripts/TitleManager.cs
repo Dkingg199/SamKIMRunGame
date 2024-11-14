b53c3332ecd67cc10d56db22a9c9ab99a2503cca
DESKTOP-G7NS9V3\user <wpghks1998@naver.com>
2024-11-14 11:23:35 +0900

    TitleManager scripts작성, 시작버튼 이미지 추가

---
+++ Scripts/TitleManager.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TitleManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private string rungameVersion = "0.1";
+    [SerializeField] private string runnickName = string.Empty;
+    [SerializeField] private Button goLobbyButton = null;
+
+    private byte maxPlayerinLobby = 4;
+
+    private void Start()
+    {
+        goLobbyButton.interactable = true;
+    }
+
+    //닉네임 없는 것 체크
+    //PhotonNetwork에 연결되었는지 확인
+    //버전 확인하고 연결
+    public void GoLobby()
+    {
+        //닉네임 없으면 게임 접속 안됨.
+        if(string.IsNullOrEmpty(runnickName))
+        {
+            Debug.Log("닉네임을 입력해주세요.");
+            return;
+        }
+
+        //IsConnected 발동하면 JoinRandomRoom() 호출
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else //
+        {
+            Debug.LogFormat("Connect : {0}", rungameVersion);
+            PhotonNetwork.GameVersion = rungameVersion;
+            //포톤 클라우드에 접속을 시작하는 지점
+            //접속에 성공하면 OnConnectedToMaster 메서드 호출
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    //InputField_NickName과 연결해 닉네임을 가져옴.
+    public void OnValueChangedNickName(string _RnickName)
+    {
+        runnickName = _RnickName;
+
+        //유저 이름 지정
+        PhotonNetwork.NickName = runnickName;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.LogFormat("Connected to Master: {0}", runnickName);
+
+        goLobbyButton.interactable = false;
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("Disconnected: {0}", cause);
+
+        goLobbyButton.interactable = true;
+
+        //방 생성 시 OnGoLobby 호출
+        Debug.Log("방 만들기");
+        PhotonNetwork.CreateRoom(
+            null,
+            new RoomOptions
+            { MaxPlayers = maxPlayerinLobby });
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("방에 입장했습니다.");
+        //각자 씬을 호출
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.LogErrorFormat("방 입장에 실패했습니다. ({0}): {1}", returnCode, message);
+
+        goLobbyButton.interactable = true;
+
+        Debug.Log("방 생성");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayerinLobby });  
+    }
+}
