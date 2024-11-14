ab24fd8110364f4e10629081fbcc4bb8db1fcc61
DESKTOP-VKT9SVB\user <ksu4250@gmail.com>
2024-11-14 09:46:53 +0900

    집에서 했던 작업(11-13 시험끝나고 집에서)

---
+++ Scripts/RunGameManager.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using UnityEngine;
+using System.Collections;
+
+public class RunGameManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject playerPrefab = null;
+    [SerializeField] private Vector3[] playerPosition = new Vector3[4];
+
+    private void Start()
+    {
+        if (playerPrefab != null)
+        {
+            //
+            DefaultPool pool = PhotonNetwork.PrefabPool as DefaultPool;
+            if (!pool.ResourceCache.ContainsKey(playerPrefab.name))
+                pool.ResourceCache.Add(playerPrefab.name, playerPrefab);
+            //
+        }
+
+        StartCoroutine(MakePlayer());
+    }
+
+    private IEnumerator MakePlayer()
+    {
+        float delay = 3f * PhotonNetwork.LocalPlayer.ActorNumber;
+
+        yield return new WaitForSeconds(delay);
+
+        GameObject go = PhotonNetwork.Instantiate(
+                playerPrefab.name,
+                playerPosition[PhotonNetwork.LocalPlayer.ActorNumber - 1],
+                Quaternion.identity,
+                0);
+    }
+}
