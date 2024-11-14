6f9684b082f96a3af68055975e52c47eff883556
DESKTOP-VKT9SVB\user <ksu4250@gmail.com>
2024-11-14 11:22:56 +0900

    카운트다운 기능 구현

--- Scripts/RunPlayerCtrl.cs
+++ Scripts/RunPlayerCtrl.cs
@@ -2,17 +2,21 @@ using Photon.Pun;
 using Unity.VisualScripting;
 using UnityEngine;
 
+// 구현 기능 : 플레이어 이동
 public class RunPlayerCtrl : MonoBehaviourPun
 {
     [SerializeField] private Vector3 moveDis = Vector3.zero;
+    private bool isControl = false;
 
     private void Awake()
     {
         moveDis = new Vector3(0.1f, 0f, 0f);
     }
+
+    // moveDis 만큼 플레이어가 이동
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isControl && Input.GetKeyDown(KeyCode.Space))
         {
             transform.Translate(moveDis);
         }
